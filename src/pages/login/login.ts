ea57bb18067bd207a2531955a63e087887d64996
ratihpratiwi <nov.raratih@gmail.com>
2017-09-14 13:48:23 +0700

    project mobile

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,6 +4,8 @@ import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {TabsPage} from '../tabs/tabs';
 import {LoginService} from '../../providers/login-service';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-login',
@@ -11,10 +13,10 @@ import {LoginService} from '../../providers/login-service';
 })
 export class LoginPage {
   data: any = {};
+  msg: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public loginService: LoginService,
-              private storage: Storage) {
-
+              private storage: Storage, public alertCtrl: AlertController) {
   }
 
   loginHandler(loginData) {
@@ -27,14 +29,16 @@ export class LoginPage {
         this.storage.set("appToken", result.token);
         this.storage.set("user", result.user);
         this.navCtrl.push(TabsPage)
-       console.log(res)
-
-
+        console.log(res)
+          let alert = this.alertCtrl.create({
+            title: result.error,
+            buttons: ['OK']
+          });
+        console.log(result.error)
+          alert.present()
       });
-    }else {
+
     }
-  }
 
-
+    }
 }
-
