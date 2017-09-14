1b5783c3b6d318b155ecb525b66738ef14ecb683
ratihpratiwi <nov.raratih@gmail.com>
2017-09-14 16:07:32 +0700

    project mobile

--- src/providers/url-master/url-master.ts
+++ src/providers/url-master/url-master.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UrlMasterProvider {
 
-static urlAPI:String="http://localhost:8083/mis";
+static urlAPI:String="http://192.168.0.190:8083/mis";
 }
