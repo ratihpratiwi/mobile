b4e9b8a5a8cee2bd9d08a52a29b4d8e83598aac2
ratihpratiwi <nov.raratih@gmail.com>
2017-09-26 09:09:51 +0700

    project mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Platform, AlertController} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
-import { Push, PushObject, PushOptions} from '@ionic-native/push';
+// import { Push, PushObject, PushOptions} from '@ionic-native/push';
 declare var FCMPlugin;
 @Component({
   templateUrl: 'app.html'
@@ -12,19 +12,18 @@ export class MyApp {
   rootPage: any = LoginPage;
 
   constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-              public alertCtrl: AlertController, public push: Push) {
+              public alertCtrl: AlertController) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       this.pushsetup();
       splashScreen.hide();
-
     });
   }
 
   pushsetup() {
     //   const options: PushOptions = {
     //     android: {
-    //       senderID: 'cp1YSQk7vfg:APA91bG99P8YVhr6LpdRf78VU3Fk0IF1VACH_0TuB3qwsNJBQDcRHYrMn2qGsBodw58D8s_DW_TbFzN9q1rj_wzkPFRz5aF8WzqVaNY1UDo89pqfIFTwB4eYNOwIlD8W_kqMggZsCb2F'
+    //       senderID: 'test'
     //     }};
     //   const pushObject: PushObject = this.push.init(options);
     //   pushObject.on('notification').subscribe((notification: any) => {
@@ -56,7 +55,9 @@ export class MyApp {
       );
       FCMPlugin.onNotification(
         function (data) {
+          console.log(data)
           if (data.wasTapped) {
+
             //Notification was received on device tray and tapped by the user.
             alert(JSON.stringify(data));
           } else {
