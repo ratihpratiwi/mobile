58ad9beb96fbc1d0e75d8c0dcd818431dfb00b73
ratihpratiwi <nov.raratih@gmail.com>
2017-09-26 09:20:27 +0700

    project mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Platform, AlertController} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
-// import { Push, PushObject, PushOptions} from '@ionic-native/push';
+import { Push, PushObject, PushOptions} from '@ionic-native/push';
 declare var FCMPlugin;
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +12,7 @@ export class MyApp {
   rootPage: any = LoginPage;
 
   constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-              public alertCtrl: AlertController) {
+              public alertCtrl: AlertController, public push:Push) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       this.pushsetup();
@@ -21,57 +21,55 @@ export class MyApp {
   }
 
   pushsetup() {
-    //   const options: PushOptions = {
-    //     android: {
-    //       senderID: 'test'
-    //     }};
-    //   const pushObject: PushObject = this.push.init(options);
-    //   pushObject.on('notification').subscribe((notification: any) => {
-    //     if (notification.additionalData.foreground) {
-    //       let youralert = this.alertCtrl.create({
-    //         title: 'New Push notification',
-    //         message: notification.message
-    //       });
-    //       youralert.present();
-    //     }
-    //   });
-    //   pushObject.on('registration').subscribe((registration: any) => {
-    //     //do whatever you want with the registration ID
-    //   });
-    //
-    //   pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
-    // }
-      FCMPlugin.onTokenRefresh(function(token){
-        alert( token );
+      const options: PushOptions = {
+        android: {
+          senderID: 'test'
+        }};
+      const pushObject: PushObject = this.push.init(options);
+      pushObject.on('notification').subscribe((notification: any) => {
+        if (notification.additionalData.foreground) {
+          let youralert = this.alertCtrl.create({
+            title: 'New Push notification',
+            message: notification.message
+          });
+          youralert.present();
+        }
       });
-      FCMPlugin.getToken(
-        function (token) {
-          console.log(token);
-          alert(token);
-        },
-        function (err) {
-          console.log('error retrieving token: ' + err);
-        }
-      );
-      FCMPlugin.onNotification(
-        function (data) {
-          console.log(data)
-          if (data.wasTapped) {
+      pushObject.on('registration').subscribe((registration: any) => {
+        //do whatever you want with the registration ID
+      });
 
-            //Notification was received on device tray and tapped by the user.
-            alert(JSON.stringify(data));
-          } else {
-            //Notification was received in foreground. Maybe the user needs to be notified.
-            alert(JSON.stringify(data));
-          }
-        },
-        function (msg) {
-          console.log('onNotification callback successfully registered: ' + msg);
-        },
-        function (err) {
-          console.log('Error registering onNotification callback: ' + err);
-        }
-      )
+      pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
+    }
+
+      // FCMPlugin.getToken(
+      //   function (token) {
+      //     console.log(token);
+      //     alert(token);
+      //   },
+      //   function (err) {
+      //     console.log('error retrieving token: ' + err);
+      //   }
+      // );
+      // FCMPlugin.onNotification(
+      //   function (data) {
+      //     console.log(data)
+      //     if (data.wasTapped) {
+      //
+      //       //Notification was received on device tray and tapped by the user.
+      //       alert(JSON.stringify(data));
+      //     } else {
+      //       //Notification was received in foreground. Maybe the user needs to be notified.
+      //       alert(JSON.stringify(data));
+      //     }
+      //   },
+      //   function (msg) {
+      //     console.log('onNotification callback successfully registered: ' + msg);
+      //   },
+      //   function (err) {
+      //     console.log('Error registering onNotification callback: ' + err);
+      //   }
+      // )
 
     }
-  }
+  // }
