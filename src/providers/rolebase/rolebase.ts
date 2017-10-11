11093ada5556327d937d837c4edb4607b19d550d
ratihpratiwi <nov.raratih@gmail.com>
2017-10-11 11:02:59 +0700

    project mobile

--- src/providers/rolebase/rolebase.ts
+++ src/providers/rolebase/rolebase.ts
@@ -1,13 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {Storage} from '@ionic/storage';
-import { Headers } from '@angular/http';
-// import {UrlMasterProvider} from '../../providers/url-master/url-master';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class RolebaseProvider {
-  private headers = new Headers({ 'precise-token': "appToken"});
   constructor(public http: Http, public storage: Storage) {
   }
 
